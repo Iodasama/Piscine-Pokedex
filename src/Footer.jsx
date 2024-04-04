c15e950b1bee2a2e468e66467063c05c807c3039
Jerome Trezeguet <jerome.trezeguet@lapiscine.pro>
2024-04-04 09:59:22 +0200

    Initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {return (
+    <div> 
+        <footer className="App-footer">
+        <p>Pokemons</p>
+        </footer> 
+    </div>
+    )}
+    
+    export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+
+    const projectTitle = "Pokemon Project X"
+    
+    return (
+    <div> 
+        <header className="App-header">
+        
+            
+        <p>{projectTitle}</p>
+        <img src= "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png" className="App-logo" alt="logo" />
+        </header> 
+
+
+    </div>
+    )}
+    
+    export default Header;
